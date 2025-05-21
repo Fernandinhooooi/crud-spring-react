a8789b75855ed725ddfd842d611e70f415e1eef5
Fernandinhooooi <161386574+Fernandinhooooi@users.noreply.github.com>
2025-05-21 17:55:42 -0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProdutoPage from './pages/ProdutoPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/produtos" element={<ProdutoPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
