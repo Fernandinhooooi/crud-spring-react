033c1e6eb2c45caa04a6d35fe635cc51134ee00b
Fernandinhooooi <161386574+Fernandinhooooi@users.noreply.github.com>
2025-05-21 17:50:00 -0300

    Create UsuarioServiceDetails.java

---
+++ ackend/backend/src/main/java/com/exemplo/demo/service/UsuarioServiceDetails.java
@@ -0,0 +1,33 @@
+package com.exemplo.demo.service;
+
+import com.exemplo.demo.model.Usuario;
+import com.exemplo.demo.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class UsuarioServiceDetails implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return new org.springframework.security.core.userdetails.User(
+                usuario.getEmail(),
+                usuario.getSenha(),
+                usuario.getRoles().stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList())
+        );
+    }
+}
